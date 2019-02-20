ce73a0aaec17ee2a532e93c46d02c66012f86222
Marcos Cordeiro <47802166+Marcos300@users.noreply.github.com>
2019-02-19 23:31:10 -0300

    Add files via upload

---
+++ minhabiblioteca.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int somarInteiro(int a,int b)
+{
+int somar; 
+somar = a + b;
+return somar;
+}
+
+int subInteiro(int a,int b)
+{
+int subtrair; 
+subtrair = a - b;
+return subtrair;
+}
+
+int multInteiro(int a,int b)
+{
+int multiplicar; 
+multiplicar = a * b;
+return multiplicar;
+}
+
