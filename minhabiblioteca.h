2e36fad0939730f6486084d0032be6799665714d
Marcos Cordeiro <47802166+Marcos300@users.noreply.github.com>
2019-02-20 00:13:24 -0300

    Update minhabiblioteca.h

--- minhabiblioteca.h
+++ minhabiblioteca.h
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <conio.h>
 
-int somarInteiro(int a,int b)
+int somarInteiros(int a,int b)
 {
 int somar; 
 somar = a + b;
 return somar;
 }
 
-int subInteiro(int a,int b)
+int subInteiros(int a,int b)
 {
 int subtrair; 
 subtrair = a - b;
 return subtrair;
 }
 
-int multInteiro(int a,int b)
+int multInteiros(int a,int b)
 {
 int multiplicar; 
 multiplicar = a * b;
